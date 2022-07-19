d93c599cfe6dc233d35a4dd1ca057a1dfa7861d8
喻小伟 <yuxiaowei@zhongan.io>
2022-07-19 11:23:26 +0800

    feat: 身份认证开发

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -86,6 +86,12 @@ const asyncRoutes: Array<RouteRecordRaw> = [
     meta: { title: '组合计划书' },
     component: () => import('@/views/proposal/compositionProposal/index.vue'),
   },
+  {
+    name: 'verify',
+    path: '/verify',
+    meta: { title: '身份认证' },
+    component: () => import('@/views/verify/index.vue'),
+  },
 ];
 
 export default asyncRoutes;
