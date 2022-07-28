78fe2654c705e5675e6914a960b83d49d3d8f7a8
za-qixuchao <qixuchao@zhongan.io>
2022-07-28 10:38:07 +0800

    feat: 调整页签功能

--- src/views/proposal/createProposal/index.vue
+++ src/views/proposal/createProposal/index.vue
@@ -2,7 +2,7 @@
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-07-14 10:14:33
  * @LastEditors: za-qixuchao qixuchao@zhongan.io
- * @LastEditTime: 2022-07-28 09:46:55
+ * @LastEditTime: 2022-07-28 10:30:31
  * @FilePath: /zat-planet-h5-cloud-insure/src/views/proposal/createProposal/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -120,7 +120,8 @@ import {
   ProposalInsuredProductItem,
   ProposalHolder,
 } from '@/api/modules/createProposal.data';
-import { insureProductDetail, ProductData } from '@/api/modules/trial';
+import { insureProductDetail } from '@/api/modules/trial';
+import { ProductData } from '@/common/constants/trial.data';
 import { SEX_LIMIT_LIST } from '@/common/constants';
 import ProductList from './components/ProductList/index.vue';
 import ProductRisk from './components/ProductRisk/index.vue';
