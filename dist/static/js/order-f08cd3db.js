c31eebfe0f4f28d1d7b2cffa22e30c605ae83d1a
王园丽 <wangyuanli@zhongan.io>
2022-08-01 16:02:34 +0800

    feat: build

---
+++ dist/static/js/order-f08cd3db.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index-a2fdfbb9.js";const a=r=>e.post("/api/app/order/v1/pageQuery",r),p=r=>e.post("/api/app/order/v1/detail",{id:r});export{p as a,a as g};
---
+++ dist/static/js/order-legacy-33d93af2.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-37c13d9a.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e.b}],execute:function(){e("g",(e=>t.post("/api/app/order/v1/pageQuery",e))),e("a",(e=>t.post("/api/app/order/v1/detail",{id:e})))}}}));
