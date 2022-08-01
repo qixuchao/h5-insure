624491c21a8f74e92b0239b72fc7abdf55789f83
za-qixuchao <qixuchao@zhongan.io>
2022-08-01 17:33:34 +0800

    feat: npm run build

--- dist/static/js/order-dcd35f94.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./index-c8f92633.js";const a=r=>e.post("/api/app/order/v1/pageQuery",r),p=r=>e.post("/api/app/order/v1/detail",{id:r});export{p as a,a as g};
---
+++ dist/static/js/proposalList-e4783f03.js
@@ -0,0 +1,1 @@
+import{b as s}from"./index-5e028fb3.js";const t="/api/app/insure/proposal",e=o=>s.post(`${t}/queryProposalProductList`,o),a=o=>s.post(`${t}/historyProposalList`,o),p=o=>s.post(`${t}/deleteProposal/${o}`),n=o=>s.post(`${t}/preview/${o}`),P=o=>s.post(`${t}/generatePdf/${o}`);export{n as a,p as d,P as g,a as h,e as q};
