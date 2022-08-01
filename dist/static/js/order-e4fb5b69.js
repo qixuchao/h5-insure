83bf1d77203139774aef7c1423605794d66c701e
za-qixuchao <qixuchao@zhongan.io>
2022-08-01 09:52:37 +0800

    feat: npm run build

--- dist/static/js/order-e4fb5b69.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./index-1de39856.js";const a=r=>e.post("/api/app/order/v1/pageQuery",r),p=r=>e.post("/api/app/order/v1/detail",{id:r});export{p as a,a as g};
--- dist/static/js/useStorage-legacy-bdaf3279.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-e5499911.js"],(function(e){"use strict";var o;return{setters:[function(e){o=e.S}],execute:function(){e("s",new o({source:"sessionStorage"})),e("l",new o({source:"localStorage"}));const s=new o({source:"cookie"});e("u",(()=>s||new o({source:"cookie"})))}}}));
