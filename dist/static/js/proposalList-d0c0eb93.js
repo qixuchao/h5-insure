3c305a2b5e06d215d1bd7567e91c91aa098ba316
za-qixuchao <qixuchao@zhongan.io>
2022-08-02 10:12:50 +0800

    feat: npm run build

--- dist/static/js/proposalList-d0c0eb93.js
+++
@@ -1,1 +0,0 @@
-import{b as s}from"./index-6ba7ac7e.js";const t="/api/app/insure/proposal",e=o=>s.post(`${t}/queryProposalProductList`,o),a=o=>s.post(`${t}/historyProposalList`,o),p=o=>s.post(`${t}/deleteProposal/${o}`),n=o=>s.post(`${t}/preview/${o}`),P=o=>s.post(`${t}/generatePdf/${o}`);export{n as a,p as d,P as g,a as h,e as q};
