28cdc67bd4cbb88e5d63d617b2a3f6791d86479f
za-qixuchao <qixuchao@zhongan.io>
2022-08-02 16:53:39 +0800

    feat: 测试问题修复

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-06-21 19:34:02
  * @LastEditors: za-qixuchao qixuchao@zhongan.io
- * @LastEditTime: 2022-08-02 15:15:28
+ * @LastEditTime: 2022-08-02 16:53:06
  * @FilePath: /zat-planet-h5-cloud-insure/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,7 +16,6 @@ import vue3PhotoPreview from 'vue3-photo-preview';
 import vconsole from 'vconsole';
 import dayjs from 'dayjs';
 import zh from 'dayjs/locale/zh-cn';
-import cookie from 'js-cookie';
 
 // pinia
 import VueDOMPurifyHTML from 'vue-dompurify-html';
@@ -31,6 +30,7 @@ import resize from '@/common/directives/resize';
 import Storage from '@/utils/storage';
 
 const storage = new Storage({ source: 'localStorage' });
+const cookie = new Storage({ source: 'cookie' });
 const start = async () => {
   if (window.location.origin.indexOf('test') !== -1) {
     // eslint-disable-next-line new-cap
@@ -45,13 +45,13 @@ const start = async () => {
       storage.set('device', cookies.device);
       storage.set('statusBarHeight', cookies.statusBarHeight);
     }
-    // if (cookies.token) {
-    //   cookie.set('token', cookies.token);
-    //   const res = await queryUserInfo();
-    //   if (res.data.code === '10000') {
-    //     storage.set('userInfo', res.data.data);
-    //   }
-    // }
+    if (cookies.token) {
+      cookie.set('token', cookies.token);
+      // const res = await queryUserInfo();
+      // if (res.data.code === '10000') {
+      //   storage.set('userInfo', res.data.data);
+      // }
+    }
   } catch (e) {
     console.log('获取cookies失败，或不是移动端');
   }
@@ -71,3 +71,5 @@ const start = async () => {
   app.directive('resize', resize);
   app.mount('#app');
 };
+
+start();
