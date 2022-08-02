923936c2d8631ffad6874bab1652caed4443c8d2
喻小伟 <yuxiaowei@zhongan.io>
2022-08-02 20:14:30 +0800

    feat: build

--- dist/static/js/useStorage-5eaf0d93.js
+++
@@ -1,1 +0,0 @@
-import{S as o}from"./index-c52e51e2.js";const c=new o({source:"sessionStorage"}),r=new o({source:"localStorage"}),e=new o({source:"cookie"}),t=()=>e||new o({source:"cookie"});export{r as l,c as s,t as u};
