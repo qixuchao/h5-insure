710584da1f7ea71a4ad1ee3c68df4717770e45a0
王园丽 <wangyuanli@zhongan.io>
2022-08-02 10:07:39 +0800

    feat: build

---
+++ dist/static/js/useStorage-baea45fc.js
@@ -0,0 +1,1 @@
+import{S as o}from"./index-650d668b.js";const c=new o({source:"sessionStorage"}),r=new o({source:"localStorage"}),e=new o({source:"cookie"}),t=()=>e||new o({source:"cookie"});export{r as l,c as s,t as u};
