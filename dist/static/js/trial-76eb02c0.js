bf077f036715635e40b123381a9ffbcc7c22e4dc
喻小伟 <yuxiaowei@zhongan.io>
2022-08-04 18:25:38 +0800

    feat: build

--- dist/static/js/trial-76eb02c0.js
+++
@@ -1,1 +0,0 @@
-import{b as i}from"./index-01f483f1.js";const p=(a={})=>i.post("/api/app/insure/product/insureProductDetail",a),r=a=>i.post("/api/app/insure/insurance/premiumCalc",a),t=(a={})=>i.post("/api/app/insure/product/insureProductDetailNew",a);export{t as a,p as i,r as p};
