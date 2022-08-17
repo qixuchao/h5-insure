c7eb8d52da7c9841bf5d8c64145d3e16d9c077ef
喻小伟 <yuxiaowei@zhongan.io>
2022-08-17 14:10:11 +0800

    feat: build

--- dist/static/js/inform-legacy-01464273.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-f3ec903c.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e.b}],execute:function(){const s="/api/app/insure/insurance";e("l",(e=>t.post(`${s}/listCustomerQuestions`,e))),e("g",(e=>t.post(`${s}/getCustomerQuestionsDetail`,e))),e("s",(e=>t.post(`${s}/saveMarketerNotices`,e)))}}}));
---
+++ dist/static/js/useStorage-legacy-bd02eb62.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-86ce05c9.js"],(function(e){"use strict";var c;return{setters:[function(e){c=e.S}],execute:function(){e("s",new c({source:"sessionStorage"})),new c({source:"localStorage"});const o=new c({source:"cookie"});e("u",(()=>o||new c({source:"cookie"})))}}}));
