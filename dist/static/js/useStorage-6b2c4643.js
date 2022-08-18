c1ab93cdd8e8606c80e1149c58ce953c1e408f45
za-qixuchao <qixuchao@zhongan.io>
2022-08-18 19:50:02 +0800

    feat: npm run build

---
+++ dist/static/js/useStorage-6b2c4643.js
@@ -0,0 +1,1 @@
+import{S as o}from"./index-b78f50a6.js";const c=new o({source:"sessionStorage"});new o({source:"localStorage"});const e=new o({source:"cookie"}),r=()=>e||new o({source:"cookie"});export{c as s,r as u};
