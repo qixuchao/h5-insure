d5b93b16d94214dbc97006afa7327408b8071005
za-wangjiaqi <wangjiaqi@zhongan.io>
2022-08-25 14:14:48 +0800

    feat: 留咨页、结果页开发

---
+++ dist/static/js/order-legacy-82169e5c.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-8178dc0b.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e.b}],execute:function(){e("g",(e=>t.post("/api/app/order/v1/pageQuery",e))),e("a",(e=>t.post("/api/app/order/v1/detail",{id:e}))),e("d",((e,r)=>t.post("/api/app/order/v1/delete",{id:e,status:r})))}}}));
