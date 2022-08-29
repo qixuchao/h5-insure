a4196af90877bc37cdd9fd6db3137301864c15dc
za-qixuchao <qixuchao@zhongan.io>
2022-08-29 18:44:16 +0800

    feat: npm run build

---
+++ dist/static/js/trial-0bf31832.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index-8e0d8448.js";const s=(a={})=>i.post("/api/app/insure/product/insureProductDetail",a),r=a=>i.post("/api/app/insure/insurance/premiumCalc",a);export{s as i,r as p};
