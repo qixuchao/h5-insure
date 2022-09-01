87074cf1f97c2b1e479d1b30fd1dc88824682fcc
liyang <liyang003@zhongan.io>
2022-09-01 14:28:07 +0800

    build: deploy

--- dist/static/js/notice-legacy-bf2e9218.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(E){"use strict";return{execute:function(){E("Q",1),E("a",2),E("H",1),E("I",2),E("N",{HOLDER_OBJECT_TYPE_ENUM:4,INSURER_OBJECT_TYPE_ENUM:5,AGENT_OBJECT_TYPE_ENUM:9})}}}));
