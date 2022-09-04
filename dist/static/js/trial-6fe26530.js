60e97889c557502c3a22a1e45aa7ff035efb975e
za-qixuchao <qixuchao@zhongan.io>
2022-09-04 23:14:00 +0800

    fix: 修复支付失败的问题

---
+++ dist/static/js/trial-6fe26530.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index-c254cb5e.js";const s=(a={})=>i.post("/api/app/insure/product/insureProductDetail",a),r=a=>i.post("/api/app/insure/insurance/premiumCalc",a);export{s as i,r as p};
--- dist/static/js/useStorage-legacy-a4ab5a30.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-2725b38f.js"],(function(e){"use strict";var o;return{setters:[function(e){o=e.S}],execute:function(){e("s",new o({source:"sessionStorage"})),new o({source:"localStorage"});const s=new o({source:"cookie"});e("u",(()=>s||new o({source:"cookie"})))}}}));
