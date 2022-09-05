9236a00eb3a83c99a3066aa9b5b3fa2bd9f54a30
za-qixuchao <qixuchao@zhongan.io>
2022-09-05 17:27:53 +0800

    feat: merge code

---
+++ dist/static/js/trial-33cbd364.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index-a8f12b11.js";const s=(a={})=>i.post("/api/app/insure/product/insureProductDetail",a),r=a=>i.post("/api/app/insure/insurance/premiumCalc",a);export{s as i,r as p};
