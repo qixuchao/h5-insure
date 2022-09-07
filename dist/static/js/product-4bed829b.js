facf95475c749680cdeb81741336fbc34abbd8f0
za-qixuchao <qixuchao@zhongan.io>
2022-09-07 15:02:29 +0800

    feat: npm run build

---
+++ dist/static/js/product-4bed829b.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-125b94ab.js";const r=t=>o.post("/api/app/insure/product/getProductDetail",t);export{r as p};
---
+++ dist/static/js/useStorage-bd1b4dcc.js
@@ -0,0 +1,1 @@
+import{S as o}from"./index-125b94ab.js";const c=new o({source:"sessionStorage"});new o({source:"localStorage"});const e=new o({source:"cookie"}),r=()=>e||new o({source:"cookie"});export{c as s,r as u};
