5653fd491a0e5540a95d832f755f01cda2d3351a
王园丽 <wangyuanli@zhongan.io>
2022-09-14 21:36:11 +0800

    feat: build

--- dist/static/js/product-a2cef5be.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./index-794221cb.js";const r=t=>o.post("/api/app/insure/product/getProductDetail",t);export{r as p};
---
+++ dist/static/js/theme-legacy-0d67cf8b.js
@@ -0,0 +1,1 @@
+System.register([],(function(e){"use strict";return{execute:function(){e("t",{primaryColor:"#ff6d23",checkboxCheckedIconColor:"#ff6d23"})}}}));
