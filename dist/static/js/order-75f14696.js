886894cba48627385f91ecac07ffdbc3078e536f
liangxiaojun <353030799@qq.com>
2022-09-15 18:46:43 +0800

    build: 构建发布

--- dist/static/js/order-75f14696.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index-934d035f.js";const a=e=>r.post("/api/app/order/v1/pageQuery",e),o=(e,t)=>r.post("/api/app/order/v1/delete",{id:e,status:t});export{o as d,a as g};
--- dist/static/js/product-4b9bc03c.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./index-934d035f.js";const r=t=>o.post("/api/app/insure/product/getProductDetail",t);export{r as p};
