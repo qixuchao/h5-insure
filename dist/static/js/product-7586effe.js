979817c03832af0cd5675ac31765ba1cca55818e
liangxiaojun <353030799@qq.com>
2022-09-16 11:10:33 +0800

    build: 重新构建发布

---
+++ dist/static/js/product-7586effe.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-274c0709.js";const r=t=>o.post("/api/app/insure/product/getProductDetail",t);export{r as p};
--- dist/static/js/product-bb46a113.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./index-927a024e.js";const r=t=>o.post("/api/app/insure/product/getProductDetail",t);export{r as p};
