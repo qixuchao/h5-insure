0c88e99e105aad1baf1d2f8ce6ffdac39a0c3ac8
王园丽 <wangyuanli@zhongan.io>
2022-09-17 14:25:07 +0800

    feat: build

---
+++ dist/static/js/product-7c779546.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-53a8b7fc.js";const r=t=>o.post("/api/app/insure/product/getProductDetail",t);export{r as p};
--- dist/static/js/product-da3f960f.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./index-f6751754.js";const r=t=>o.post("/api/app/insure/product/getProductDetail",t);export{r as p};
