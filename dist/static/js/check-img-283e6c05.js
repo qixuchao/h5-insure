3ad1e6825e965a795db023c8773af1b3739f7d57
王园丽 <wangyuanli@zhongan.io>
2022-09-17 19:26:20 +0800

    feat: build

--- dist/static/js/check-img-283e6c05.js
+++
@@ -1,1 +0,0 @@
-var a="/static/assets/check-img.171aabb1.png";export{a as c};
--- dist/static/js/check-img-legacy-6aa347ee.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(t){"use strict";return{execute:function(){t("c","/static/assets/check-img.171aabb1.png")}}}));
