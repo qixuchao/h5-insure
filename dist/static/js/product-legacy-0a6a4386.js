882d028c788019ddae4154a6d6a1c2d86d755aea
王园丽 <wangyuanli@zhongan.io>
2022-09-22 20:48:52 +0800

    feat: build

---
+++ dist/static/js/product-legacy-0a6a4386.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-b1242d80.js"],(function(t){"use strict";var e;return{setters:[function(t){e=t.b}],execute:function(){t("p",(t=>e.post("/api/app/insure/product/getProductDetail",t)))}}}));
