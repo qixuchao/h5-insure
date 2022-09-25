35d1ee8f8483697baa24aeab316b2e77491ee58f
za-qixuchao <qixuchao@zhongan.io>
2022-09-25 15:12:06 +0800

    feat: 调整分享逻辑

--- start.sh
+++ start.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-###
- # @Author: za-qixuchao qixuchao@zhongan.io
- # @Date: 2022-09-01 16:10:34
- # @LastEditors: za-qixuchao qixuchao@zhongan.io
- # @LastEditTime: 2022-09-23 19:38:02
- # @FilePath: /zat-planet-h5-cloud-insure/start.sh
- # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-###
-# PLANET_ENV=dev
+# DEPLOY_ENV=dev
 # GATEWAY_PROJECT_ID=141604
-if [ $PLANET_ENV = 'dev' ]
+if [ $DEPLOY_ENV = 'dev' ]
 then
   # 修改: Ship 开发环境 -> 应用编排 -> 高级配置 -> 环境变量, 保存并生效后等待服务重启
   # GATEWAY_PROJECT_ID 开发环境后端应用 ID
@@ -19,8 +11,8 @@ then
   COMMANDER_URL="$([ ${COMMANDER_API_URL} ] && echo ${COMMANDER_API_URL} || echo "http:\/\/$([ ${COMMANDER_PROJECT_ID} ] && echo "${COMMANDER_PROJECT_ID}-" || echo "")aquarius-commander.test.za-tech.net")"
 else
   # test/pre/prd 环境
-  API_URL="http:\/\/zat-planet-gateway.${PLANET_ENV}.za-tech.net"
-  COMMANDER_URL="http:\/\/aquarius-commander.${PLANET_ENV}.za-tech.net"
+  API_URL="http:\/\/zat-planet-gateway.${DEPLOY_ENV}.za-tech.net"
+  COMMANDER_URL="http:\/\/aquarius-commander.${DEPLOY_ENV}.za-tech.net"
 fi
 
 # replace
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-06-21 19:34:02
  * @LastEditors: za-qixuchao qixuchao@zhongan.io
- * @LastEditTime: 2022-09-23 15:32:45
+ * @LastEditTime: 2022-09-25 14:14:57
  * @FilePath: /zat-planet-h5-cloud-insure/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -48,8 +48,8 @@ export default defineConfig((env) => {
           // target: 'http://zat-planet-gateway.test.za-tech.net',
           // target: 'http://142461-zat-planet-gateway.test.za-tech.net',
           // target: 'http://zat-planet-gateway.test.za-tech.net',
-          target: 'http://155382-zat-planet-gateway.test.za-tech.net',
-          // target: 'http://zat-planet-gateway.test.za-tech.net',
+          // target: 'http://155382-zat-planet-gateway.test.za-tech.net',
+          target: 'http://zat-planet-gateway.test.za-tech.net',
           changeOrigin: true, // 允许跨域
         },
       },
