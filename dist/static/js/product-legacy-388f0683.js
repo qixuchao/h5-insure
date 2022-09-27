f2a92f41743bf73532b1664012be4e7ec1019f45
王园丽 <wangyuanli@zhongan.io>
2022-09-27 17:48:31 +0800

    fix: 手机号查询订单，保单信息可以修改

---
+++ dist/static/js/product-legacy-388f0683.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-d93edb40.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e.b}],execute:function(){e("p",(e=>t.post("/api/app/insure/product/getProductDetail",e)))}}}));
