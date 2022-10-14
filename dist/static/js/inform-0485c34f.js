ad782f80700a09fe9868135ee623e7100595c041
za-yangfan <yangfan001@zhongan.io>
2022-10-14 16:20:04 +0800

    build: deploy

--- dist/static/js/inform-0485c34f.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-1be2f5bc.js";const t="/api/app/insure/insurance",r=s=>e({url:`${t}/listCustomerQuestions`,method:"POST",data:s}),i=s=>e({url:`${t}/getCustomerQuestionsDetail`,method:"POST",data:s}),u=s=>e({url:`${t}/saveMarketerNotices`,method:"POST",data:s});export{i as g,r as l,u as s};
--- dist/static/js/product-legacy-6ef32620.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-0dba231e.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e.r}],execute:function(){e("p",(e=>t({url:"/api/app/insure/product/getProductDetail",method:"POST",data:e})))}}}));
