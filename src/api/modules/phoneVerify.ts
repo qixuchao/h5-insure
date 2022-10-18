15960ee4e297a1262931549235f2619180adbd99
liangxiaojun <353030799@qq.com>
2022-10-18 20:22:52 +0800

    fix: 接口参数params接收问题

--- src/api/modules/phoneVerify.ts
+++ src/api/modules/phoneVerify.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-08-23 13:55:39
- * @LastEditors: za-qixuchao qixuchao@zhongan.io
- * @LastEditTime: 2022-09-28 11:42:45
+ * @LastEditors: kevin.liang
+ * @LastEditTime: 2022-10-18 20:19:28
  * @FilePath: /zat-planet-h5-cloud-insure/src/api/modules/phoneVerify.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,9 +11,9 @@ import request from '../request';
 // 发送短信
 export const sendCode = (mobile: string) => {
   return request<ResponseData>({
-    url: '/api/app/insure/insurance/sendSmsCodeToC',
+    url: `/api/app/insure/insurance/sendSmsCodeToC`,
     method: 'POST',
-    data: { params: { mobile } },
+    params: { mobile },
   });
 };
 
@@ -21,6 +21,6 @@ export const checkCode = (mobile: string, smsCode: string) => {
   return request<ResponseData<boolean>>({
     url: '/api/app/insure/insurance/checkSmsCode',
     method: 'POST',
-    data: { params: { mobile, smsCode } },
+    params: { mobile, smsCode },
   });
 };
