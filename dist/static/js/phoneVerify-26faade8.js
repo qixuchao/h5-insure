8a947dcc943f7710e06cbda0bae14662cb3f546f
liangxiaojun <353030799@qq.com>
2022-10-18 17:26:05 +0800

    build: 修复bug,构建发布

---
+++ dist/static/js/phoneVerify-26faade8.js
@@ -0,0 +1,1 @@
+import{r as a}from"./index-00d5bb1b.js";const n=e=>a({url:"/api/app/insure/insurance/sendSmsCodeToC",method:"POST",data:{params:{mobile:e}}}),o=(e,r)=>a({url:"/api/app/insure/insurance/checkSmsCode",method:"POST",data:{params:{mobile:e,smsCode:r}}});export{o as c,n as s};
--- dist/static/js/product-7b012094.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-46b727c2.js";const o=r=>t({url:"/api/app/insure/product/getProductDetail",method:"POST",data:r});export{o as p};
