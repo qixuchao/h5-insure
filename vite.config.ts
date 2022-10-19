c3530226c5b477dd4e14ede9356c3f3ae908dd6e
王园丽 <wangyuanli@zhongan.io>
2022-10-19 16:49:32 +0800

    fix: 带出的客户信息不可修改，其他可以修改

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig((env) => {
           // target: 'http://zat-planet-gateway.test.za-tech.net',
           // target: 'http://142461-zat-planet-gateway.test.za-tech.net',
           // target: 'http://155382-zat-planet-gateway.test.za-tech.net',
-          target: 'http://zat-planet-gateway.test.za-tech.net',
+          target: 'http://142461-zat-planet-gateway.test.za-tech.net',
           changeOrigin: true, // 允许跨域
         },
       },
