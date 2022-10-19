a8821b0e17c9596818cde390faedfc25984da024
王园丽 <wangyuanli@zhongan.io>
2022-10-19 21:10:50 +0800

    build: 构建

---
+++ dist/static/js/useStorage-legacy-b355f2ab.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-949c500c.js"],(function(e){"use strict";var c;return{setters:[function(e){c=e.S}],execute:function(){e("s",new c({source:"sessionStorage"})),new c({source:"localStorage"});const o=new c({source:"cookie"});e("u",(()=>o||new c({source:"cookie"})))}}}));
