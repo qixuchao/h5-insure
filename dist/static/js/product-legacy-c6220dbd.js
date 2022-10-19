bcbb92a6450eab7630aed0d986aabe61d6e01273
za-qixuchao <qixuchao@zhongan.com>
2022-10-19 10:26:17 +0800

    feat: npm run build

---
+++ dist/static/js/product-legacy-c6220dbd.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-63939d5d.js"],(function(t){"use strict";var e;return{setters:[function(t){e=t.r}],execute:function(){t("p",(t=>e({url:"/api/app/insure/product/getProductDetail",method:"POST",data:t})))}}}));
