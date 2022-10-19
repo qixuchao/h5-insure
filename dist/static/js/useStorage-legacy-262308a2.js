07a96368bc0c198d5cacf7fee157c213638e2472
za-qixuchao <qixuchao@zhongan.com>
2022-10-19 18:47:28 +0800

    feat: npm run build

---
+++ dist/static/js/useStorage-legacy-262308a2.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-a503c237.js"],(function(e){"use strict";var o;return{setters:[function(e){o=e.S}],execute:function(){e("s",new o({source:"sessionStorage"})),new o({source:"localStorage"});const s=new o({source:"cookie"});e("u",(()=>s||new o({source:"cookie"})))}}}));
