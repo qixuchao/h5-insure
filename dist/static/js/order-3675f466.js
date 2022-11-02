bd7e5385acf518d7dab587e7c2dea270747557cc
wuchao <wuchao@zhongan.io>
2022-11-02 20:44:54 +0800

    feat: 编译

---
+++ dist/static/js/order-3675f466.js
@@ -0,0 +1,1 @@
+import{r}from"./index-ac3587c6.js";const d=(e={})=>r({url:"/api/app/order/v1/pageQuery",method:"POST",data:e}),o=(e,t)=>r({url:"/api/app/order/v1/delete",method:"POST",data:{id:e,status:t}});export{o as d,d as g};
---
+++ dist/static/js/useStorage-01bc2b33.js
@@ -0,0 +1,1 @@
+import{S as o}from"./index-ac3587c6.js";const c=new o({source:"sessionStorage"});new o({source:"localStorage"});const e=new o({source:"cookie"}),r=()=>e||new o({source:"cookie"});export{c as s,r as u};
