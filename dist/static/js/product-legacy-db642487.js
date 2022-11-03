00d17976524e1f1454c52621fda7ad406f511206
wuchao <wuchao@zhongan.io>
2022-11-03 17:17:18 +0800

    feat: 编译

--- dist/static/js/product-legacy-db642487.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-0e1bee9f.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e.r}],execute:function(){e("p",(e=>t({url:"/api/app/insure/product/getProductDetail",method:"POST",data:e})))}}}));
