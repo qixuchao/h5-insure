7cfe2e8a73fb8cc0ef3b903074705679f1442802
wuchao <wuchao@zhongan.io>
2022-11-06 19:07:59 +0800

    feat: 构建

---
+++ dist/static/js/order-4762a304.js
@@ -0,0 +1,1 @@
+import{r}from"./index-60c4c699.js";const d=(e={})=>r({url:"/api/app/order/v1/pageQuery",method:"POST",data:e}),o=(e,t)=>r({url:"/api/app/order/v1/delete",method:"POST",data:{id:e,status:t}});export{o as d,d as g};
--- dist/static/js/product-c3c26ae7.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-ff28915f.js";const o=r=>t({url:"/api/app/insure/product/getProductDetail",method:"POST",data:r});export{o as p};
