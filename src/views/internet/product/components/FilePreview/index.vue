953c1877af00286ea6409c6cd7d20e950f630275
za-zhaopu <zhaopu@zhongan.io>
2022-11-10 20:46:34 +0800

    fix: 修复资料滚动条问题

--- src/views/internet/product/components/FilePreview/index.vue
+++ src/views/internet/product/components/FilePreview/index.vue
@@ -2,7 +2,7 @@
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-09-15 17:44:21
  * @LastEditors: zhaopu
- * @LastEditTime: 2022-11-10 20:41:25
+ * @LastEditTime: 2022-11-10 20:45:20
  * @FilePath: /zat-planet-h5-cloud-insure/src/views/chuangxin/baigebao/product/components/FIlePreview/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -167,12 +167,9 @@ watch(
           e.readDisabled = false;
         });
       }
-      if (previewRef.value) {
-        previewRef.value.scrollTop = 0;
-        setTimeout(() => {
-          calcuateFlg.value = true;
-        }, 1000);
-      }
+      setTimeout(() => {
+        calcuateFlg.value = true;
+      }, 1000);
     }
   },
   {
