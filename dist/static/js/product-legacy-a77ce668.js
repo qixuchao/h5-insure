da0f70ce66410aee4dd7118cf137f039e4895b4e
za-zhaopu <zhaopu@zhongan.io>
2022-11-10 17:59:47 +0800

    build: deploy

--- dist/static/js/product-legacy-a77ce668.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-359da06f.js"],(function(t){"use strict";var e;return{setters:[function(t){e=t.r}],execute:function(){t("p",(t=>e({url:"/api/app/insure/product/getProductDetail",method:"POST",data:t})))}}}));
