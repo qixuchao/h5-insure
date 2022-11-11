f9967740a828e65c3a8986e621ab73a844359303
za-zhaopu <zhaopu@zhongan.io>
2022-11-11 18:23:03 +0800

    build: deploy

---
+++ dist/static/js/phoneVerify-7160ea27.js
@@ -0,0 +1,1 @@
+import{r}from"./index-39a1c9eb.js";const n=e=>r({url:"/api/app/insure/insurance/sendSmsCodeToC",method:"POST",params:{mobile:e}}),o=(e,s)=>r({url:"/api/app/insure/insurance/checkSmsCode",method:"POST",params:{mobile:e,smsCode:s}});export{o as c,n as s};
---
+++ dist/static/js/product-89ba0392.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-39a1c9eb.js";const o=r=>t({url:"/api/app/insure/product/getProductDetail",method:"POST",data:r});export{o as p};
