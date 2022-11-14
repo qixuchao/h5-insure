5795267fcde56a8844fceb72ca3a80a32734c9dd
za-zhaopu <zhaopu@zhongan.io>
2022-11-14 12:47:16 +0800

    build: deploy

--- dist/static/js/order-9334be43.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-4214a2e6.js";const d=(e={})=>r({url:"/api/app/order/v1/pageQuery",method:"POST",data:e}),o=(e,t)=>r({url:"/api/app/order/v1/delete",method:"POST",data:{id:e,status:t}});export{o as d,d as g};
---
+++ dist/static/js/useStorage-legacy-f2c574b9.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-9e84ad02.js"],(function(e){"use strict";var o;return{setters:[function(e){o=e.S}],execute:function(){e("s",new o({source:"sessionStorage"})),new o({source:"localStorage"});const s=new o({source:"cookie"});e("u",(()=>s||new o({source:"cookie"})))}}}));
