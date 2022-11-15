274da79c160caffed31134432ba9e618ceccb761
wuchao <wuchao@zhongan.io>
2022-11-15 11:07:15 +0800

    feat: 修改nginx配置

--- dist/static/js/product-legacy-b9a8ee74.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-664c3732.js"],(function(t){"use strict";var e;return{setters:[function(t){e=t.r}],execute:function(){t("p",(t=>e({url:"/api/app/insure/product/getProductDetail",method:"POST",data:t})))}}}));
