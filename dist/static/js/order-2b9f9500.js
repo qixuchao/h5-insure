980ba48ce6bb5094bdd92061f8527b9945a90cbb
za-zhaopu <zhaopu@zhongan.io>
2022-11-18 19:16:51 +0800

    build: deploy

--- dist/static/js/order-2b9f9500.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-3fb0ff1d.js";const d=(e={})=>r({url:"/api/app/order/v1/pageQuery",method:"POST",data:e}),o=(e,t)=>r({url:"/api/app/order/v1/delete",method:"POST",data:{id:e,status:t}});export{o as d,d as g};
--- dist/static/js/product-c9d6857a.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-3fb0ff1d.js";const o=r=>t({url:"/api/app/insure/product/getProductDetail",method:"POST",data:r});export{o as p};
