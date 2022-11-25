8756aa9b182de6822a90e199402509f9bcb436f2
za-zhaopu <zhaopu@zhongan.io>
2022-11-25 20:56:59 +0800

    fix: 修复投保流程冒烟问题

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,7 +2,7 @@
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-07-14 11:44:33
  * @LastEditors: zhaopu
- * @LastEditTime: 2022-11-25 15:46:44
+ * @LastEditTime: 2022-11-25 20:44:14
  * @FilePath: /zat-planet-h5-cloud-insure/src/router/routes.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -214,19 +214,19 @@ const templateRoutes = [
     component: () => import('@/views/template/product/detail/netsale.vue'),
   },
   {
-    name: '赠险',
+    name: '赠险预览',
     path: '/template/preview/free',
     meta: { title: '' },
     component: () => import('@/views/template/product/preview/free.vue'),
   },
   {
-    name: '一年期',
+    name: '一年期预览',
     path: '/template/preview/short',
     meta: { title: '' },
     component: () => import('@/views/template/product/preview/short.vue'),
   },
   {
-    name: '网电销',
+    name: '网电销预览',
     path: '/template/preview/netsale',
     meta: { title: '' },
     component: () => import('@/views/template/product/preview/netsale.vue'),
