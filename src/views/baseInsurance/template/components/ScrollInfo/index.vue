0738c309c42ac35cab6465ba0704c381d950b666
za-zhaopu <zhaopu@zhongan.io>
2022-11-26 13:20:41 +0800

    feat: 添加基线投保流程页面

---
+++ src/views/baseInsurance/template/components/ScrollInfo/index.vue
@@ -0,0 +1,151 @@
+<!--
+ * @Author: wangyuanli@zhongan.io
+ * @Date: 2022-09-17 16:00
+ * @LastEditors:  wangyuanli@zhongan.io
+ * @LastEditTime: 2022-09-17 16:00
+ * @Description: 审核版首页
+-->
+<template>
+  <ProScrollTab class="tabs" :list="tabList" sticky scrollspy>
+    <template #tab1>
+      <slot name="form" />
+    </template>
+    <template #tab2>
+      <div class="spec">
+        <img
+          v-for="(item, index) in props.detail?.tenantProductInsureVO?.spec || []"
+          :key="index"
+          :src="item"
+          class="detail-img"
+        />
+        <ProDivider />
+      </div>
+      <ProCard title="产品资料">
+        <template #subTitle>
+          <div class="sub-title">产品资料文件详情可手动放大，以便您更清晰查阅内容。</div>
+        </template>
+        <div class="tab-1-content">
+          请查看
+          <ProPDFviewer
+            v-for="(item, index) in detail?.tenantProductInsureVO?.attachmentVOList || []"
+            :key="index"
+            :type="queryFilePerfix(item.attachmentUri)"
+            class="file-name"
+            :title="getFileName(item.attachmentName)"
+            :content="item.attachmentUri"
+          >
+            >
+            <span v-if="index !== (detail?.tenantProductInsureVO?.attachmentVOList || []).length - 1" class="dun-hao"
+              >、</span
+            >
+          </ProPDFviewer>
+        </div>
+      </ProCard>
+    </template>
+    <template #tab3>
+      <ProCard title="理赔流程">
+        <ProTimeline :list="props.detail?.tenantProductInsureVO?.settlementProcessList" />
+      </ProCard>
+    </template>
+    <template #tab4>
+      <ProCard title="常见问题">
+        <Question :list="props.detail?.tenantProductInsureVO?.questionList" />
+      </ProCard>
+    </template>
+  </ProScrollTab>
+</template>
+
+<script lang="ts" setup>
+import ProCard from '@/components/ProCard/index.vue';
+import ProDivider from '@/components/ProDivider/index.vue';
+import ProScrollTab from '@/components/ProScrollTab/index.vue';
+import ProTimeline from '@/components/ProTimeline/index.vue';
+import Question from '../Question/index.vue';
+
+const props = defineProps({
+  isCheck: {
+    type: Boolean,
+    default: false,
+  },
+  url: {
+    type: String,
+    default: '',
+  },
+  detail: {
+    type: Object,
+    default: () => {},
+  },
+});
+const tabList = ref<Array<{ title: string; slotName: string }>>([
+  {
+    title: '我要投保',
+    slotName: 'tab1',
+  },
+  {
+    title: '产品特色',
+    slotName: 'tab2',
+  },
+  {
+    title: '理赔流程',
+    slotName: 'tab3',
+  },
+  {
+    title: '常见问题',
+    slotName: 'tab4',
+  },
+]);
+
+const queryFilePerfix = (fileUrl: string) => {
+  const fileType: string = fileUrl.split('?')[0];
+  const index = fileType.lastIndexOf('.');
+  return fileType.substring(index + 1);
+};
+
+// const queryFileType = (fileUrl: string) => {
+//   const fileType = queryFilePerfix(fileUrl);
+//   if ('pdf' || 'Pdf') return 'pdf';
+//   if('')
+// };
+
+const getFileName = (fileName: string): string => {
+  if (!fileName) return '';
+  let newName = fileName;
+  if (newName.indexOf('《') === -1) newName = `《${newName}`;
+  if (newName.indexOf('》') === -1) newName = `${newName}》`;
+  return newName;
+};
+</script>
+
+<style lang="scss" scoped>
+.spec {
+  img {
+    width: 100%;
+  }
+}
+
+.sub-title {
+  font-size: 26px;
+  font-weight: 400;
+  color: #99a9c0;
+  line-height: 38px;
+  margin-bottom: 10px;
+}
+.tab-1-content {
+  font-size: 26px;
+  font-weight: 400;
+  color: #393d46;
+  line-height: 38px;
+  margin-top: 25px;
+  margin-bottom: 40px;
+}
+
+// 覆盖ProTimeline样式
+:deep(.com-time-line .item .left .num) {
+  background: var(--van-primary-background-color);
+  border: 1px solid $primary-color;
+  &::after {
+    border-left: 1px dashed var(--van-primary-background-color);
+    border-right: 1px dashed var(--van-primary-background-color);
+  }
+}
+</style>
---
+++ src/views/baseInsurance/theme.ts
@@ -0,0 +1,5 @@
+export default {
+  primaryColor: '#0d6efe',
+  primaryBackgroundColor: '#0d6efe20', // 自定义的的背景色
+  checkboxCheckedIconColor: '#0d6efe', // van-checkbox的背景色
+};
