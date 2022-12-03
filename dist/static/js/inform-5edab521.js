fa73381b75b60dd52e164a1e90f13c62960b95af
za-qixuchao <qixuchao@zhongan.com>
2022-12-03 19:43:29 +0800

    feat: npm run build

---
+++ dist/static/js/inform-5edab521.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-b395e119.js";const s="/api/app/insure/insurance",t=t=>e({url:`${s}/listCustomerQuestions`,method:"POST",data:t}),a=t=>e({url:`${s}/getCustomerQuestionsDetail`,method:"POST",data:t}),r=t=>e({url:`${s}/saveMarketerNotices`,method:"POST",data:t});export{a as g,t as l,r as s};
--- dist/static/js/useStorage-edcca054.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-5502edb8.js";const o=new e({source:"sessionStorage"}),s=new e({source:"localStorage"}),r=new e({source:"cookie"}),c=()=>r||new e({source:"cookie"});export{s as l,o as s,c as u};
