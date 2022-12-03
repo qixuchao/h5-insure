d722241ae39c1444377390ae84149f54384646b3
za-qixuchao <qixuchao@zhongan.com>
2022-12-03 19:19:23 +0800

    feat: npm run build

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,7 +2,7 @@
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-07-14 11:44:33
  * @LastEditors: za-qixuchao qixuchao@zhongan.com
- * @LastEditTime: 2022-12-03 19:12:32
+ * @LastEditTime: 2022-12-03 19:17:04
  * @FilePath: /zat-planet-h5-cloud-insure/src/router/routes.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -210,7 +210,7 @@ const baseInsurance = [
   {
     name: 'base一年期',
     path: '/baseInsurance/short',
-    meta: { title: '', keepAlive: true, requireWxJs: tryOnMounted },
+    meta: { title: '', keepAlive: true, requireWxJs: true },
     component: () => import('@/views/baseInsurance/templates/short.vue'),
   },
   {
