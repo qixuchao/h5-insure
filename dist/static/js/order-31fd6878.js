d184a05b8567b56f8ed1003358bb4cc8bb82d476
za-qixuchao <qixuchao@zhongan.com>
2022-12-04 17:29:38 +0800

    feat: npm run build

---
+++ dist/static/js/order-31fd6878.js
@@ -0,0 +1,1 @@
+import{r as a}from"./index-3dbd1683.js";const d=(d={})=>a({url:"/api/app/order/v1/pageQuery",method:"POST",data:d}),e=(d,e)=>a({url:"/api/app/order/v1/delete",method:"POST",data:{id:d,status:e}});export{e as d,d as g};
--- dist/static/js/useStorage-2a8b8451.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-e450ab26.js";const o=new e({source:"sessionStorage"}),s=new e({source:"localStorage"}),r=new e({source:"cookie"}),a=()=>r||new e({source:"cookie"});export{s as l,o as s,a as u};
