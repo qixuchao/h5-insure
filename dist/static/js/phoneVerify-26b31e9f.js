a6dabec64b2600d130e817f64801190e72d9bc45
liangxiaojun <353030799@qq.com>
2022-12-05 14:29:06 +0800

    build: 构建发布

---
+++ dist/static/js/phoneVerify-26b31e9f.js
@@ -0,0 +1,1 @@
+import{r}from"./index-5d3893c3.js";const n=e=>r({url:"/api/app/insure/insurance/sendSmsCodeToC",method:"POST",params:{mobile:e}}),o=(e,s)=>r({url:"/api/app/insure/insurance/checkSmsCode",method:"POST",params:{mobile:e,smsCode:s}});export{o as c,n as s};
