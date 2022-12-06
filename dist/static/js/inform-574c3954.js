d4f5ee1f2c4aae46edfe8293f010010cf143c094
wuchao <wuchao@zhongan.io>
2022-12-06 16:28:09 +0800

    feat: 修复调整空白页

--- dist/static/js/inform-574c3954.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-a58e4b72.js";const s="/api/app/insure/insurance",t=t=>e({url:`${s}/listCustomerQuestions`,method:"POST",data:t}),a=t=>e({url:`${s}/getCustomerQuestionsDetail`,method:"POST",data:t}),r=t=>e({url:`${s}/saveMarketerNotices`,method:"POST",data:t});export{a as g,t as l,r as s};
--- dist/static/js/useStorage-a73db9b4.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-a58e4b72.js";const o=new e({source:"sessionStorage"}),s=new e({source:"localStorage"}),r=new e({source:"cookie"}),a=()=>r||new e({source:"cookie"});export{s as l,o as s,a as u};
