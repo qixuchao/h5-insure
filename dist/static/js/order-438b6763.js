658401788cfa9ae5fdedeefab7a51114bb4fd461
za-qixuchao <qixuchao@zhongan.com>
2022-12-07 12:30:47 +0800

    feat: npm run build

--- dist/static/js/order-438b6763.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./index-013d70a0.js";const d=(d={})=>a({url:"/api/app/order/v1/pageQuery",method:"POST",data:d}),e=(d,e)=>a({url:"/api/app/order/v1/delete",method:"POST",data:{id:d,status:e}});export{e as d,d as g};
