0671ccde1ffc08bf494b9daaeed8b75b56a5c24d
zhaopu <zhaopu@zhongan.io>
2022-12-07 13:24:19 +0800

    build: deploy

---
+++ dist/static/js/useStorage-legacy-239fa2ea.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-38b8b0bc.js"],(function(e){"use strict";var o;return{setters:[function(e){o=e.S}],execute:function(){e("s",new o({source:"sessionStorage"})),e("l",new o({source:"localStorage"}));const s=new o({source:"cookie"});e("u",(()=>s||new o({source:"cookie"})))}}}));
