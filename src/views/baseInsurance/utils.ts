f7da11f3800e70b37245f0d27c295a5408b361ee
wuchao <wuchao@zhongan.io>
2022-12-08 16:26:19 +0800

    feat: 构建

--- src/views/baseInsurance/utils.ts
+++ src/views/baseInsurance/utils.ts
@@ -777,7 +777,7 @@ export const freeTransform = (o: any) => {
     orderCategory: '1', // 1 '1' // 订单类型
     tenantOrderHolder: {
       tenantId: o.tenantId,
-      certType: o.order.tenantOrderHolder?.certEndType || '1', // 默认身份证
+      certType: o.order.tenantOrderHolder?.certType || CERT_TYPE_ENUM.CERT, // 默认身份证
       ...o.order.tenantOrderHolder,
     },
     extInfo: {
@@ -791,7 +791,7 @@ export const freeTransform = (o: any) => {
       {
         ...o.order.tenantOrderInsuredList[0],
         tenantId: o.tenantId,
-        certType: o.order.tenantOrderInsuredList?.[0]?.certEndType || '1', // 默认身份证
+        certType: o.order.tenantOrderInsuredList?.[0]?.certType || CERT_TYPE_ENUM.CERT, // 默认身份证
         mobile:
           o.order.tenantOrderInsuredList[0].relationToHolder === RELATION_HOLDER_ENUM.SELF
             ? o.order.tenantOrderHolder.mobile
