f480f3902bfa7146ea39bcd52478d1ccebc3c69d
za-qixuchao <qixuchao@zhongan.com>
2022-12-08 14:04:08 +0800

    feat: 调整网电销的路由名

--- dist/static/js/theme-legacy-a8152f06.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("t",{primaryColor:"#0d6efe",primaryBackgroundColor:"#0d6efe20",checkboxCheckedIconColor:"#0d6efe"})}}}));
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-07-14 11:44:33
- * @LastEditors: zhaopu
- * @LastEditTime: 2022-12-07 14:25:35
+ * @LastEditors: za-qixuchao qixuchao@zhongan.com
+ * @LastEditTime: 2022-12-08 14:03:25
  * @FilePath: /zat-planet-h5-cloud-insure/src/router/routes.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -221,7 +221,7 @@ const baseInsurance = [
   },
   {
     name: 'base网电销详情',
-    path: '/baseInsurance/netsaleDetail',
+    path: '/baseInsurance/netDetail',
     meta: { title: '', keepAlive: true, requireWxJs: true },
     component: () => import('@/views/baseInsurance/templates/netSale/detail.vue'),
   },
