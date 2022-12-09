ba00c04ebcf9d0c85b94299093387edca50222b0
zhaopu <zhaopu@zhongan.io>
2022-12-09 18:30:47 +0800

    build: deploy

--- dist/static/js/order-5a320b49.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./index-e427a2b7.js";const e=(e={})=>a({url:"/api/app/order/v1/pageQuery",method:"POST",data:e}),r=(e,r)=>a({url:"/api/app/order/v1/delete",method:"POST",data:{id:e,status:r}});export{r as d,e as g};
