c0dbf3e97cc051c81d5ed4c091e242d9798d61e4
wuchao <wuchao@zhongan.io>
2022-12-12 15:08:21 +0800

    feat: 构建

--- dist/static/js/order-1c542c08.js
+++
@@ -1,1 +0,0 @@
-import{B as a}from"./index-dd407709.js";const d=(d={})=>a({url:"/api/app/order/v1/pageQuery",method:"POST",data:d}),e=(d,e)=>a({url:"/api/app/order/v1/delete",method:"POST",data:{id:d,status:e}});export{e as d,d as g};
