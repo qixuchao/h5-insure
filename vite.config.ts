1e9d63786e8b7db236b6305b8eb0ae5b5ad68748
liangxiaojun <353030799@qq.com>
2022-12-13 13:14:46 +0800

    feat: 合并代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 /*
  * @Author: za-qixuchao qixuchao@zhongan.com
  * @Date: 2022-11-28 17:42:39
-<<<<<<< HEAD
  * @LastEditors: kevin.liang
- * @LastEditTime: 2022-12-13 13:12:03
-=======
- * @LastEditors: zhaopu
- * @LastEditTime: 2022-12-13 11:26:33
->>>>>>> a5b826cdf624bd89ad180e7a7096db2574195fe3
+ * @LastEditTime: 2022-12-13 13:14:32
  * @FilePath: /zat-planet-h5-cloud-insure/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
