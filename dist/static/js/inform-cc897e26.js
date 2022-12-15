edc013ebcb536d2f3880f31e11f821427c4bec37
zhaopu <zhaopu@zhongan.io>
2022-12-15 14:16:08 +0800

    build: deploy

--- dist/static/js/inform-cc897e26.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./index-1e3aef1e.js";const s="/api/app/insure/insurance",t=t=>e({url:`${s}/listCustomerQuestions`,method:"POST",data:t}),a=t=>e({url:`${s}/getCustomerQuestionsDetail`,method:"POST",data:t}),o=t=>e({url:`${s}/saveMarketerNotices`,method:"POST",data:t});export{a as g,t as l,o as s};
