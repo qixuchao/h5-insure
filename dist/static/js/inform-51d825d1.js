cee076b6b41dc1f48b7316f4808e7a1f118e6998
liangxiaojun <353030799@qq.com>
2022-12-22 18:14:33 +0800

    build: 构建发布

--- dist/static/js/inform-51d825d1.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./index-a63027e0.js";const s="/api/app/insure/insurance",t=t=>e({url:`${s}/listCustomerQuestions`,method:"POST",data:t}),a=t=>e({url:`${s}/getCustomerQuestionsDetail`,method:"POST",data:t}),o=t=>e({url:`${s}/saveMarketerNotices`,method:"POST",data:t});export{a as g,t as l,o as s};
