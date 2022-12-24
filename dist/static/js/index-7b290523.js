e9fefe1c8b390bf5e46121f5f7bba97a0eacba68
za-qixuchao <qixuchao@zhongan.com>
2022-12-24 19:32:50 +0800

    feat: npm run build

---
+++ dist/static/js/index-7b290523.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./logo-e4245f5a.js";import{d as s,b as o,c as t,e,w as r,i}from"./index-f7b0041e.js";import"./pdfdist-3849db64.js";const l=i("img",{src:a,alt:"logo",style:{margin:"100px auto",width:"200px",display:"block"}},null,-1),p=s({setup:a=>(a,s)=>{const i=o("ProPageWrap");return t(),e(i,{"main-class":"page-home"},{default:r((()=>[l])),_:1})}});export{p as default};
