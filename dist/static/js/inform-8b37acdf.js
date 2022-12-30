b0a5a89fd7e6014137dfdf3c3863ef8657016d3a
zhaopu <zhaopu@zhongan.io>
2022-12-30 19:18:21 +0800

    build: deploy

---
+++ dist/static/js/inform-8b37acdf.js
@@ -0,0 +1,1 @@
+import{A as e}from"./index-5d1ce8b8.js";const s="/api/app/insure/insurance",t=t=>e({url:`${s}/listCustomerQuestions`,method:"POST",data:t}),a=t=>e({url:`${s}/getCustomerQuestionsDetail`,method:"POST",data:t}),o=t=>e({url:`${s}/saveMarketerNotices`,method:"POST",data:t});export{a as g,t as l,o as s};
---
+++ dist/static/js/useStorage-legacy-0346df50.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-9497d129.js"],(function(e){"use strict";var o;return{setters:[function(e){o=e.S}],execute:function(){e("s",new o({source:"sessionStorage"})),e("l",new o({source:"localStorage"}));const s=new o({source:"cookie"});e("u",(()=>s||new o({source:"cookie"})))}}}));
