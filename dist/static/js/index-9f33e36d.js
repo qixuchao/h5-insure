6f0ce66eb4e44a1d5d4631a967745ecfed201a7e
zhaopu <zhaopu@zhongan.io>
2022-12-30 14:50:18 +0800

    build: deploy

--- dist/static/js/index-9f33e36d.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,d as t,c as a,g as e,i as d,t as i,a8 as o}from"./index-463deff3.js";import"./pdfh5-e5c0db2a.js";const c={class:"com-custom-card"},l={class:"title"};var r=s(t({props:{title:{type:String,required:!0}},setup:s=>(t,r)=>(a(),e("div",c,[d("div",l,i(s.title),1),d("div",null,[o(t.$slots,"default",{},void 0,!0)])]))}),[["__scopeId","data-v-059c21ee"]]);export{r as default};
