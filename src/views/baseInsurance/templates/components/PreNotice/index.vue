1480b61b0cdd21966ae617ce880a11ce679ed46e
za-qixuchao <qixuchao@zhongan.com>
2023-01-04 15:06:21 +0800

    fix: 修复投保流程前置告知弹窗弹出失败的问题

--- src/views/baseInsurance/templates/components/PreNotice/index.vue
+++ src/views/baseInsurance/templates/components/PreNotice/index.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-09-15 15:01:12
- * @LastEditors: zhaopu
- * @LastEditTime: 2023-01-03 15:15:24
+ * @LastEditors: za-qixuchao qixuchao@zhongan.com
+ * @LastEditTime: 2023-01-04 15:02:41
  * @FilePath: /zat-planet-h5-cloud-insure/src/views/chuangxin/baigebao/product/components/PreNotice/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -14,7 +14,7 @@
       <p>
         请仔细阅读免责条款、投保须知等信息，请您重点阅读并知晓
         <span class="file-name" @click="(e) => clickHandler(e, 'privacyAgreement')">
-          《{{ state.insureConfig?.privacyAgreement.title || '用户隐私协议' }}》
+          《{{ state.insureConfig.privacyAgreement?.title || '用户隐私协议' }}》
         </span>
         ，为维护您的合法权益，您的操作轨迹将被记录。
       </p>
