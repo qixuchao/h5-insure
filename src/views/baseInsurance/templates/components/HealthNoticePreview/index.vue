6f20bbe2623dff459561ae809591ca8f725050bb
zhaopu <zhaopu@zhongan.io>
2023-01-10 14:48:54 +0800

    fix: 删除健告按钮边框

--- src/views/baseInsurance/templates/components/HealthNoticePreview/index.vue
+++ src/views/baseInsurance/templates/components/HealthNoticePreview/index.vue
@@ -2,7 +2,7 @@
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-09-15 17:44:21
  * @LastEditors: zhaopu
- * @LastEditTime: 2023-01-10 14:45:38
+ * @LastEditTime: 2023-01-10 14:48:21
  * @FilePath: /zat-planet-h5-cloud-insure/src/views/chuangxin/baigebao/product/components/FIlePreview/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -177,7 +177,6 @@ watch(
     padding: 16px 20px;
     .van-button--primary {
       background: var(--van-linear-bg) !important;
-      border-color: $primary-color;
       font-size: 24px;
     }
 
