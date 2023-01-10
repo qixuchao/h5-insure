70cdc02d04bf34ced86339f8f3e7d2332311c119
zhaopu <zhaopu@zhongan.io>
2023-01-10 10:56:33 +0800

    fix: 修复健告按钮样式

--- src/views/baseInsurance/templates/components/HealthNoticePreview/index.vue
+++ src/views/baseInsurance/templates/components/HealthNoticePreview/index.vue
@@ -2,7 +2,7 @@
  * @Author: za-qixuchao qixuchao@zhongan.io
  * @Date: 2022-09-15 17:44:21
  * @LastEditors: zhaopu
- * @LastEditTime: 2023-01-09 19:15:55
+ * @LastEditTime: 2023-01-10 10:55:28
  * @FilePath: /zat-planet-h5-cloud-insure/src/views/chuangxin/baigebao/product/components/FIlePreview/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -185,6 +185,7 @@ watch(
       color: $primary-color;
       background-color: #ffffff;
       margin-left: 30px;
+      width: 50%;
     }
 
     .left {
