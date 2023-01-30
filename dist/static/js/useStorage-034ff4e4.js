56bb7ad379df6112832fc6afb4726761a6d6769b
zhaopu <zhaopu@zhongan.io>
2023-01-30 16:18:37 +0800

    build: deploy

--- dist/static/js/useStorage-034ff4e4.js
+++
@@ -1,1 +0,0 @@
-import{S as o}from"./index-0194c398.js";const e=new o({source:"sessionStorage"}),s=new o({source:"localStorage"}),c=new o({source:"cookie"}),r=()=>c||new o({source:"cookie"});export{s as l,e as s,r as u};
