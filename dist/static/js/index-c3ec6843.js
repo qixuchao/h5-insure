145ff7ac9faf8451582662280458375b7544cd54
za-qixuchao <qixuchao@zhongan.com>
2023-01-30 20:27:10 +0800

    feat: npm run build

--- dist/static/js/index-c3ec6843.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,d as a,c as t,g as d,i as e,t as i,aA as o}from"./index-69adfa77.js";import"./pdfh5-e5c0db2a.js";const c={class:"com-custom-card"},l={class:"title"};var r=s(a({props:{title:{type:String,required:!0}},setup:s=>(a,r)=>(t(),d("div",c,[e("div",l,i(s.title),1),e("div",null,[o(a.$slots,"default",{},void 0,!0)])]))}),[["__scopeId","data-v-059c21ee"]]);export{r as default};
