07ba021166caefbf5a21d112bbb13136f051451a
za-qixuchao <qixuchao@zhongan.com>
2023-02-06 14:45:29 +0800

    feat: npm run build

---
+++ dist/static/js/order-legacy-65732215.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-4dedd1d6.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e.A}],execute:function(){e("g",((e={})=>t({url:"/api/app/order/v1/pageQuery",method:"POST",data:e}))),e("d",((e,d)=>t({url:"/api/app/order/v1/delete",method:"POST",data:{id:e,status:d}})))}}}));
