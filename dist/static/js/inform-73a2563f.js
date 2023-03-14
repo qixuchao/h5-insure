7519872ab9fe8f8532725f87d7af3ede33584d9d
za-qixuchao <qixuchao@zhongan.com>
2023-03-15 00:36:43 +0800

    feat: npm run build

--- dist/static/js/inform-73a2563f.js
+++
@@ -1,1 +0,0 @@
-import{K as e}from"./index-5aeb3407.js";const s="/api/app/insure/insurance",t=t=>e({url:`${s}/listCustomerQuestions`,method:"POST",data:t}),a=t=>e({url:`${s}/getCustomerQuestionsDetail`,method:"POST",data:t}),o=t=>e({url:`${s}/saveMarketerNotices`,method:"POST",data:t});export{a as g,t as l,o as s};
