85fb7b93710ae63cbf8da648fe1abbb152f78cab
liangxiaojun <353030799@qq.com>
2023-03-23 17:44:25 +0800

    fix: 保障计划两个计划时写死的宽度

--- dist/static/js/index-748c053c.js
+++
@@ -1,1 +0,0 @@
-import{d as s,c as r,e,k as a,I as n}from"./index-481ac274.js";const o=s({__name:"index",props:{url:{default:""}},setup(s){const o=s;return console.log("props",o),(o,t)=>{const c=n;return s.url?(r(),e(c,{key:0,width:"100%",class:"com-template-banner",src:s.url},null,8,["src"])):a("",!0)}}});export{o as _};
---
+++ dist/static/js/inform-155c9ac1.js
@@ -0,0 +1,1 @@
+import{K as a}from"./index-ea1a3c3a.js";const e="/api/app/insure/insurance",s=s=>a({url:`${e}/listCustomerQuestions`,method:"POST",data:s}),t=s=>a({url:`${e}/getCustomerQuestionsDetail`,method:"POST",data:s}),o=s=>a({url:`${e}/saveMarketerNotices`,method:"POST",data:s});export{t as g,s as l,o as s};
