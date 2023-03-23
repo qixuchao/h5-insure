410799c5bb4b7e7c6d9759cee3d08422917452e9
za-wusiqi <wusiqi@zhongan.io>
2023-03-23 13:58:33 +0800

    fix: 豁免险的逻辑更新

---
+++ dist/static/js/order-d81a8c52.js
@@ -0,0 +1,1 @@
+import{K as a}from"./index-ec0e4382.js";const e=(e={})=>a({url:"/api/app/order/v1/pageQuery",method:"POST",data:e}),d=(e,d)=>a({url:"/api/app/order/v1/delete",method:"POST",data:{id:e,status:d}});export{d,e as g};
