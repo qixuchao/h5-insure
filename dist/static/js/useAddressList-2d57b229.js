6fa5fd59b6c2225e8b97c96d83c44aa7c06f8566
za-wusiqi <wusiqi@zhongan.io>
2023-03-23 15:02:13 +0800

    fix: 豁免险的逻辑更新

---
+++ dist/static/js/useAddressList-2d57b229.js
@@ -0,0 +1,1 @@
+import{q as o}from"./product-c48bb16b.js";import{aE as a}from"./index-481ac274.js";const t=(t,c,n)=>{a((()=>{(async()=>{try{const{code:a,data:n}=await o(t);if("10000"===a){const o={};n.forEach((a=>{o[a.relationCode]||(o[a.relationCode]=[]),o[a.relationCode].push(a)})),null==c||c(o)}}catch(a){console.log(a),null==n||n(!1)}})()}))};export{t as u};
