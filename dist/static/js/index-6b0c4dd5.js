691feb21ab3fd9e635508f323917386e4d5c1db8
za-yangfan <yangfan001@zhongan.io>
2023-03-24 19:15:28 +0800

    build: deploy

--- dist/static/js/index-6b0c4dd5.js
+++
@@ -1,1 +0,0 @@
-import{d as s,c as e,e as r,k as a,I as n}from"./index-6eca3ebc.js";const o=s({__name:"index",props:{url:{default:""}},setup(s){const o=s;return console.log("props",o),(o,t)=>{const c=n;return s.url?(e(),r(c,{key:0,width:"100%",class:"com-template-banner",src:s.url},null,8,["src"])):a("",!0)}}});export{o as _};
