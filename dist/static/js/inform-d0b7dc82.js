05cd746005f657e9194068fcbe5df593c2fe43d0
jjmwinner <jjmwinner@163.com>
2023-03-28 14:40:12 +0800

    feat: build

--- dist/static/js/inform-d0b7dc82.js
+++
@@ -1,1 +0,0 @@
-import{K as e}from"./index-b25e28f3.js";const s="/api/app/insure/insurance",t=t=>e({url:`${s}/listCustomerQuestions`,method:"POST",data:t}),a=t=>e({url:`${s}/getCustomerQuestionsDetail`,method:"POST",data:t}),o=t=>e({url:`${s}/saveMarketerNotices`,method:"POST",data:t});export{a as g,t as l,o as s};
--- dist/static/js/useAddressList-00f3ccb6.js
+++
@@ -1,1 +0,0 @@
-import{q as o}from"./product-669418c7.js";import{aE as a}from"./index-b25e28f3.js";const t=(t,c,e)=>{a((()=>{(async()=>{try{const{code:a,data:e}=await o(t);if("10000"===a){const o={};e.forEach((a=>{o[a.relationCode]||(o[a.relationCode]=[]),o[a.relationCode].push(a)})),null==c||c(o)}}catch(a){console.log(a),null==e||e(!1)}})()}))};export{t as u};
