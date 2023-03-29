65a3cdf6fb0a2544ee4dd81fd9ae2bee7727ff37
za-yangfan <yangfan001@zhongan.io>
2023-03-29 18:38:28 +0800

    build: deploy

---
+++ dist/static/js/inform-32896605.js
@@ -0,0 +1,1 @@
+import{K as a}from"./index-d9a51ae5.js";const e="/api/app/insure/insurance",s=s=>a({url:`${e}/listCustomerQuestions`,method:"POST",data:s}),t=s=>a({url:`${e}/getCustomerQuestionsDetail`,method:"POST",data:s}),o=s=>a({url:`${e}/saveMarketerNotices`,method:"POST",data:s});export{t as g,s as l,o as s};
