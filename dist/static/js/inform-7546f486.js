3e1e13994bedc3bcc3b3408e59fadba6984a0236
za-qixuchao <qixuchao@zhongan.com>
2023-03-31 14:06:58 +0800

    feat: npm run build

--- dist/static/js/inform-7546f486.js
+++
@@ -1,1 +0,0 @@
-import{K as a}from"./index-c9ae1a47.js";const e="/api/app/insure/insurance",s=s=>a({url:`${e}/listCustomerQuestions`,method:"POST",data:s}),t=s=>a({url:`${e}/getCustomerQuestionsDetail`,method:"POST",data:s}),o=s=>a({url:`${e}/saveMarketerNotices`,method:"POST",data:s});export{t as g,s as l,o as s};
---
+++ dist/static/js/useAddressList-6c8b952c.js
@@ -0,0 +1,1 @@
+import{q as o}from"./product-141cdbce.js";import{aE as a}from"./index-3224a24f.js";const t=(t,c,e)=>{a((()=>{(async()=>{try{const{code:a,data:e}=await o(t);if("10000"===a){const o={};e.forEach((a=>{o[a.relationCode]||(o[a.relationCode]=[]),o[a.relationCode].push(a)})),null==c||c(o)}}catch(a){console.log(a),null==e||e(!1)}})()}))};export{t as u};
