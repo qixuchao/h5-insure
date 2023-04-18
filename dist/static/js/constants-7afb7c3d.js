2ced76df2a9b200ace7efb384108d0d54fc9367d
za-wusiqi <wusiqi@zhongan.io>
2023-04-18 16:19:58 +0800

    feat: 动态数据更新

---
+++ dist/static/js/constants-7afb7c3d.js
@@ -0,0 +1,1 @@
+const N={TRIAL_PREMIUM:"trial",QUESTION_NOTICE:"questionNotice",INFO_COLLECTION:"infoCollection",INFO_PREVIEW:"infoPreview",SIGN:"sign",RESULT:"paymentResult"},E={TRIAL_PREMIUM:"EVENT_LONG_saveOrder",QUESTION_NOTICE:"EVENT_LONG_saveNotice",INFO_COLLECTION:"EVENT_LONG_saveInfo",INFO_PREVIEW:"EVENT_LONG_underWrite",SIGN:"EVENT_LONG_pay"};export{E as B,N as P};
