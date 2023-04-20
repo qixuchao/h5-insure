8c5998c31a3a0eded5aed01f4150c5f5967448a7
liangxiaojun <353030799@qq.com>
2023-04-20 17:19:08 +0800

    feat: 投保预览根据租户ID来

--- src/views/_template/index.vue
+++ src/views/_template/index.vue
@@ -2,6 +2,6 @@
   <ProPageWrap main-class="page-【这里要修改成页面名称】"> 模板页面 ,页面内容包裹到这个组件里面 </ProPageWrap>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup name="组件名称"></script>
 
 <style lang="scss"></style>
