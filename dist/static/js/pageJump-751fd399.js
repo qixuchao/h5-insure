71f88315188dbbc425e632f4f5a6776c8709f7cd
za-wusiqi <wusiqi@zhongan.io>
2023-04-21 14:58:25 +0800

    fix:  优化试算页面的逻辑

--- dist/static/js/pageJump-751fd399.js
+++
@@ -1,1 +0,0 @@
-import{q as a,H as p}from"./index-477a8908.js";var r=(r,s)=>{a.push({path:p[r],query:s})};export{r as p};
---
+++ dist/static/js/pageJump-7b41827a.js
@@ -0,0 +1,1 @@
+import{q as a,H as p}from"./index-127f38a4.js";var r=(r,s)=>{a.push({path:p[r],query:s})};export{r as p};
