9fb5b80960e8fc17465c5e4e9595be44f57a896a
za-qixuchao <qixuchao@zhongan.com>
2023-04-23 17:33:53 +0800

    feat: npm run build

--- dist/static/js/createProposal-fa2c9f0a.js
+++
@@ -1,1 +0,0 @@
-import{q as a}from"./index-b01c8bff.js";const r=r=>a({url:"/api/app/insure/proposal/addOrUpdateProposal",method:"POST",data:r}),p=(r={})=>a({url:`/api/app/insure/proposal/queryProposalDetailInsurer/${r.id}`,params:r,method:"GET"}),o=(r={})=>a({url:"/api/app/insure/product/v2/listInsureProductDetail",method:"POST",data:r});export{r as a,p as b,o as q};
--- dist/static/js/useAddressList-883e6aea.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./product-e0ea0287.js";import{o as a}from"./index-b01c8bff.js";const t=(t,c,e)=>{a((()=>{(async()=>{try{const{code:a,data:e}=await o(t);if("10000"===a){const o={};e.forEach((a=>{o[a.relationCode]||(o[a.relationCode]=[]),o[a.relationCode].push(a)})),null==c||c(o)}}catch(a){console.log(a),null==e||e(!1)}})()}))};export{t as u};
