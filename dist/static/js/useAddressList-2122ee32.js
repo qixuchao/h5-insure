f18bc94e2a260ccbd51a3f3dcb01ca9add9d1fd4
za-wusiqi <wusiqi@zhongan.io>
2023-04-23 16:52:07 +0800

    fix: 后端修改结构导致的问题

--- dist/static/js/useAddressList-2122ee32.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./product-b5b52262.js";import{o as t}from"./index-dd15e12e.js";const a=(a,e,c)=>{t((()=>{(async()=>{try{const{code:t,data:c}=await o(a);if("10000"===t){const o={};c.forEach((t=>{o[t.relationCode]||(o[t.relationCode]=[]),o[t.relationCode].push(t)})),null==e||e(o)}}catch(t){console.log(t),null==c||c(!1)}})()}))};export{a as u};
