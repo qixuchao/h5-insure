b5a08d8114b7efc74161f2895b6eb9335e04730d
za-yangfan <yangfan001@zhongan.io>
2023-04-23 11:05:23 +0800

    build: deploy

--- dist/static/js/useAddressList-31f6aea1.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./product-cca7be1d.js";import{o as a}from"./index-7e7dd0d0.js";const t=(t,c,e)=>{a((()=>{(async()=>{try{const{code:a,data:e}=await o(t);if("10000"===a){const o={};e.forEach((a=>{o[a.relationCode]||(o[a.relationCode]=[]),o[a.relationCode].push(a)})),null==c||c(o)}}catch(a){console.log(a),null==e||e(!1)}})()}))};export{t as u};
