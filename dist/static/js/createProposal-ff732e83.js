8321a405982a97d7518045b33d4d0d863fb25d78
za-yangfan <yangfan001@zhongan.io>
2023-04-24 11:26:50 +0800

    build: deploy

--- dist/static/js/createProposal-ff732e83.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/static/js/createProposal-26596701.js
-import{q as a}from"./index-4b1b5b56.js";const r=r=>a({url:"/api/app/insure/proposal/addOrUpdateProposal",method:"POST",data:r}),p=(r={})=>a({url:`/api/app/insure/proposal/queryProposalDetailInsurer/${r.id}`,params:r,method:"GET"}),o=(r={})=>a({url:"/api/app/insure/product/v2/listInsureProductDetail",method:"POST",data:r});export{r as a,p as b,o as q};
-========
-import{q as a}from"./index-3c7e3218.js";const r=r=>a({url:"/api/app/insure/proposal/addOrUpdateProposal",method:"POST",data:r}),p=(r={})=>a({url:`/api/app/insure/proposal/queryProposalDetailInsurer/${r.id}`,params:r,method:"GET"}),o=(r={})=>a({url:"/api/app/insure/product/v2/listInsureProductDetail",method:"POST",data:r});export{r as a,p as b,o as q};
->>>>>>>> ad430bdd168e316860f2a753ff36433fea9fb637:dist/static/js/createProposal-ff732e83.js
--- dist/static/js/index-2fff8ab2.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/static/js/index-90dcbc9d.js
-import{G as s,d as a,c as t,A as e,f as d,E as i,k as o}from"./index-4b1b5b56.js";import"./pdfh5-008ca364.js";const c={class:"com-custom-card"},l={class:"title"};var r=s(a({__name:"index",props:{title:{type:String,required:!0}},setup:s=>(a,r)=>(t(),e("div",c,[d("div",l,i(s.title),1),d("div",null,[o(a.$slots,"default",{},void 0,!0)])]))}),[["__scopeId","data-v-059c21ee"]]);export{r as default};
-========
-import{G as s,d as a,c as t,A as e,f as d,E as i,k as c}from"./index-3c7e3218.js";import"./pdfh5-008ca364.js";const o={class:"com-custom-card"},l={class:"title"};var r=s(a({__name:"index",props:{title:{type:String,required:!0}},setup:s=>(a,r)=>(t(),e("div",o,[d("div",l,i(s.title),1),d("div",null,[c(a.$slots,"default",{},void 0,!0)])]))}),[["__scopeId","data-v-059c21ee"]]);export{r as default};
->>>>>>>> ad430bdd168e316860f2a753ff36433fea9fb637:dist/static/js/index-2fff8ab2.js
