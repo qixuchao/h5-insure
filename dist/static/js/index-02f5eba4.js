f8766001bb9a9013104bb4bdab035b8f4fbb010d
za-wusiqi <wusiqi@zhongan.io>
2023-05-08 10:53:33 +0800

    fix: 后端结构改造哦

--- dist/static/js/index-02f5eba4.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/static/js/index-02f5eba4.js
-import{d as a,b as s,c as e,e as t,w as o,f as p}from"./index-a540e387.js";import"./pdfh5-008ca364.js";const l=p("img",{src:"/static/png/logo-c907ee91.png",alt:"logo",style:{margin:"100px auto",width:"200px",display:"block"}},null,-1),n=a({__name:"index",setup:a=>(a,p)=>{const n=s("ProPageWrap");return e(),t(n,{"main-class":"page-home"},{default:o((()=>[l])),_:1})}});export{n as default};
-========
-import{d as a,b as s,c as e,e as t,w as o,f as p}from"./index-d6f83575.js";import"./pdfh5-008ca364.js";const l=p("img",{src:"/static/png/logo-c907ee91.png",alt:"logo",style:{margin:"100px auto",width:"200px",display:"block"}},null,-1),n=a({__name:"index",setup:a=>(a,p)=>{const n=s("ProPageWrap");return e(),t(n,{"main-class":"page-home"},{default:o((()=>[l])),_:1})}});export{n as default};
->>>>>>>> 1e7e89018 (feat: npm run build):dist/static/js/index-ab4d1843.js
