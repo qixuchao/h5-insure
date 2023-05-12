e3a8134d937ae0da998500aec661a4b8c60621a3
za-yangfan <yangfan001@zhongan.io>
2023-05-12 10:17:34 +0800

    build: deploy

--- dist/static/js/phoneVerify-36525e17.js
+++
@@ -1,1 +0,0 @@
-import{q as a}from"./index-77a7631b.js";const s=s=>a({url:"/api/app/insure/insurance/sendSmsCodeToC",method:"POST",params:{mobile:s}}),e=s=>a({url:"/api/mall/app/v2/login/toCSendSmsCode",method:"POST",params:{mobile:s}}),o=(s,e)=>a({url:"/api/app/insure/insurance/checkSmsCode",method:"POST",params:{mobile:s,smsCode:e}});export{e as a,o as c,s};
