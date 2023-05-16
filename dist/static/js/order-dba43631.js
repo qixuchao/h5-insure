6166249c05986cf01c75225c198ae72ee705cd4d
za-yangfan <yangfan001@zhongan.io>
2023-05-17 01:39:14 +0800

    build: deploy

---
+++ dist/static/js/order-dba43631.js
@@ -0,0 +1,1 @@
+import{q as a}from"./index-f5601786.js";const e=(e={})=>a({url:"/api/app/order/v1/pageQuery",method:"POST",data:e}),d=(e,d)=>a({url:"/api/app/order/v1/delete",method:"POST",data:{id:e,status:d}});export{d,e as g};
