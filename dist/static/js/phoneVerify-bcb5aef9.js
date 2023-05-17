c3c05380eb4eb84e9dd0145f42d17534a281bd4c
za-qixuchao <qixuchao@zhongan.com>
2023-05-17 16:03:26 +0800

    feat: npm run build

---
+++ dist/static/js/phoneVerify-bcb5aef9.js
@@ -0,0 +1,1 @@
+import{q as e}from"./index-5e1fe5ed.js";const a=a=>e({url:"/api/app/insure/insurance/sendSmsCodeToC",method:"POST",params:{mobile:a}}),s=a=>e({url:"/api/mall/app/v2/login/toCSendSmsCode",method:"POST",params:{mobile:a}}),o=(a,s)=>e({url:"/api/app/insure/insurance/checkSmsCode",method:"POST",params:{mobile:a,smsCode:s}});export{s as a,o as c,a as s};
