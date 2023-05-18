03ed3c623b918b39eea124aebd187f2c71eeaee0
za-yangfan <yangfan001@zhongan.io>
2023-05-18 22:03:16 +0800

    fix: 修复长期险利益演示滚动问题

---
+++ src/views/baseInsurance/templates/components/Benefit/BenefitTable.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="benefit-table">
+    <ProTable :columns="columns" :data="tableData" :is-clone="true" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ColumnProps } from '@/components/ProTable/types';
+import ProTable from './ProTable.vue';
+import { isNotEmptyArray } from '@/common/constants/utils';
+
+interface Props {
+  dataSource: {
+    headers: string[];
+    dataList: Array<string[]>;
+  };
+}
+
+const props = defineProps<Props>();
+
+const colSpans = [
+  // {
+  //   rowIndex: 1,
+  //   spans: 4,
+  //   colIndex: 0,
+  //   contentIndex: 0, // 合并后的列采用第几行的数据
+  //   direction: 'row',
+  //   bgColor: '#ccc',
+  //   color: 'red',
+  // },
+
+  {
+    rowIndex: 5,
+    spans: 2,
+    colIndex: 2,
+    contentIndex: 5, // 合并后的列采用第几行的数据
+    direction: 'col',
+    bgColor: '#e1e1e1',
+    color: 'black',
+  },
+];
+const renderWidth = 320; // 表格在页面渲染的宽度
+// 构造列和行数据（header_是因为header里面的头是汉字，不能用于dom的ID）
+const columns = computed<ColumnProps[]>(() => {
+  if (!isNotEmptyArray(props.dataSource.headers)) {
+    return [];
+  }
+  // 表头的列数
+  return props.dataSource.headers.map((h, i) => {
+    return {
+      title: h,
+      key: `header_${i}`,
+      fixed: i < 2,
+    };
+  });
+});
+const tableData = computed(() => {
+  if (!isNotEmptyArray(props.dataSource.dataList)) {
+    return [];
+  }
+  return props.dataSource.dataList.map((row) => {
+    const rowData: { [key: string]: string } = {};
+    row.forEach((col, index) => {
+      rowData[`header_${index}`] = col;
+    });
+    return rowData;
+  });
+});
+</script>
+<style>
+.benefit-table {
+  height: 600px;
+  /* border: 1px solid red; */
+  flex: auto;
+  padding-bottom: 20px;
+  overflow: hidden;
+}
+</style>
---
+++ src/views/baseInsurance/templates/components/Benefit/ProTable.vue
@@ -0,0 +1,95 @@
+<template>
+  <div v-if="data && data.length" class="pro-table-wrap">
+    <Table v-model:translate-y="state.translateY" :columns="columns" :data="data" @update:need-fixed="setNeedFixed" />
+    <Table
+      v-if="state.needFixed"
+      v-model:translate-y="state.translateY"
+      class="pro-table-fixed"
+      style="width: 160px"
+      :columns="fixedColumns"
+      :data="data"
+    />
+  </div>
+  <div v-else class="no-data-wrap">
+    <img src="@/assets/images/empty.png" alt="暂无数据" />
+    <span>暂无数据～</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+import Table from './Table.vue';
+import { isNotEmptyArray } from '@/common/constants/utils';
+
+const tableBodyRef = ref(null);
+const tableHeadRef = ref(null);
+
+const ProTableKey = 'PRO_TABLE_KEY';
+
+const props = defineProps({
+  data: {
+    type: Array,
+    default: () => [],
+  },
+  columns: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const state = reactive({
+  translateY: 0,
+  translateX: 0,
+  offsetX: 0,
+  offsetY: 0,
+  needFixed: false,
+});
+
+provide(ProTableKey, {
+  state,
+});
+
+const fixedColumns = computed(() => {
+  if (isNotEmptyArray(props.columns)) {
+    return props.columns.filter((item) => item.fixed);
+  }
+  return [];
+});
+
+const setNeedFixed = (val) => {
+  state.needFixed = val;
+};
+</script>
+
+<style lang="scss" scoped>
+.pro-table-wrap {
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+}
+
+.pro-table-fixed {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 12;
+  box-shadow: 2px 0 8px #ddd;
+}
+
+.no-data-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 60px;
+  img {
+    width: 200px;
+    height: 200px;
+    margin-bottom: 32px;
+  }
+  span {
+    font-size: $zaui-font-size;
+    color: #888888;
+    margin-left: 10px;
+  }
+}
+</style>
