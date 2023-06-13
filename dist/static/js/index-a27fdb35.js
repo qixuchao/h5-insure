3d3b24efbb9fa314c671cae6b8f1bf394f907614
wuchao <wuchao@zhongan.io>
2023-06-13 12:01:28 +0800

    feat: 编译

---
+++ dist/static/js/index-a27fdb35.js
@@ -0,0 +1,1 @@
+import{G as s,d as a,b as t,c as e,i as d,E as i,k as l}from"./index-a1136dfb.js";const o={class:"com-custom-card"},c={class:"title"};var r=s(a({__name:"index",props:{title:{type:String,required:!0}},setup:s=>(a,r)=>(t(),e("div",o,[d("div",c,i(s.title),1),d("div",null,[l(a.$slots,"default",{},void 0,!0)])]))}),[["__scopeId","data-v-059c21ee"]]);export{r as default};
