92514079d5edc54bbaf951531f7a6bb725b63c8c
wuchao <wuchao@zhongan.io>
2023-06-26 14:17:38 +0800

    feat: 编译

--- dist/static/js/phoneVerify-7509be32.js
+++
@@ -1,1 +0,0 @@
-import{s}from"./index-c1d07286.js";const a=a=>s({url:"/api/app/insure/insurance/sendSmsCodeToC",method:"POST",params:{mobile:a}}),e=a=>s({url:"/api/mall/app/v2/login/toCSendSmsCode",method:"POST",params:{mobile:a}}),o=(a,e)=>s({url:"/api/app/insure/insurance/checkSmsCode",method:"POST",params:{mobile:a,smsCode:e}});export{e as a,o as c,a as s};
