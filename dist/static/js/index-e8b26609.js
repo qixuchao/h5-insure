866f9ab1f50a093158a2cb98a55cd06a587bc96a
za-qixuchao <qixuchao@zhongan.com>
2023-06-29 15:19:51 +0800

    feat: npm run build

---
+++ dist/static/js/index-e8b26609.js
@@ -0,0 +1,1 @@
+import{d as a,b as s,c,i as d,$ as e}from"./index-70bfd36e.js";const t={class:"product-desc-wrap"},o={class:"product-name"},r={class:"product-desc"},p=a({name:"productDesc"}),u=a({...p,props:{dataSource:{default:()=>({})}},setup:a=>(a,p)=>(s(),c("div",t,[d("div",o,e(a.dataSource.title),1),d("p",r,e(a.dataSource.productDesc),1)]))});export{u as default};
