505f31d29efe4924f2988ae8a1463a9c61305c74
jjmwinner <jjmwinner@163.com>
2023-07-18 17:51:33 +0800

    build: deploy

--- dist/static/js/index-54ab9bdf.js
+++
@@ -1,1 +0,0 @@
-import{ag as a,d as s,b as e,c as t,i as d,$ as i,ae as c}from"./index-a248c81d.js";const l={class:"com-custom-card"},o={class:"title"};var r=a(s({__name:"index",props:{title:{type:String,required:!0}},setup:a=>(s,r)=>(e(),t("div",l,[d("div",o,i(a.title),1),d("div",null,[c(s.$slots,"default",{},void 0,!0)])]))}),[["__scopeId","data-v-059c21ee"]]);export{r as default};
