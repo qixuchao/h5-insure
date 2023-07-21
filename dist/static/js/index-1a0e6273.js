10192527fd183a617141dd7eeef4e41e850e219f
wuchao <wuchao@zhongan.io>
2023-07-21 18:43:38 +0800

    feat: 编译

---
+++ dist/static/js/index-1a0e6273.js
@@ -0,0 +1,1 @@
+import{ah as a,d as s,b as e,c as t,i as d,$ as i,af as c}from"./index-4c507abe.js";const l={class:"com-custom-card"},o={class:"title"};var r=a(s({__name:"index",props:{title:{type:String,required:!0}},setup:a=>(s,r)=>(e(),t("div",l,[d("div",o,i(a.title),1),d("div",null,[c(s.$slots,"default",{},void 0,!0)])]))}),[["__scopeId","data-v-059c21ee"]]);export{r as default};
--- dist/static/js/index-386cdd9c.js
+++
@@ -1,1 +0,0 @@
-import{ah as a,d as s,b as t,c as e,i as d,$ as i,af as c}from"./index-f9d3bc6a.js";const l={class:"com-custom-card"},o={class:"title"};var r=a(s({__name:"index",props:{title:{type:String,required:!0}},setup:a=>(s,r)=>(t(),e("div",l,[d("div",o,i(a.title),1),d("div",null,[c(s.$slots,"default",{},void 0,!0)])]))}),[["__scopeId","data-v-059c21ee"]]);export{r as default};
