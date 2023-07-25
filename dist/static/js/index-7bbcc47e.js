69e76475cb6b0d18f48d976fe616d5f7af5b07e9
za-qixuchao <qixuchao@zhongan.com>
2023-07-25 10:47:56 +0800

    feat: npm run build

---
+++ dist/static/js/index-7bbcc47e.js
@@ -0,0 +1,1 @@
+import{ah as s,d as a,b as t,c as e,i as d,$ as i,af as c}from"./index-b03f65fc.js";const l={class:"com-custom-card"},o={class:"title"};var r=s(a({__name:"index",props:{title:{type:String,required:!0}},setup:s=>(a,r)=>(t(),e("div",l,[d("div",o,i(s.title),1),d("div",null,[c(a.$slots,"default",{},void 0,!0)])]))}),[["__scopeId","data-v-059c21ee"]]);export{r as default};
