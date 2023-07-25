1b3b21f856bf6a4c1cbeedf904e699286fb4d2d5
jjmwinner <jjmwinner@163.com>
2023-07-25 14:34:07 +0800

    build: deploy

---
+++ dist/static/js/phoneVerify-b0b30954.js
@@ -0,0 +1,1 @@
+import{q as a}from"./index-bc87a4b5.js";const s=s=>a({url:"/api/app/insure/insurance/sendSmsCodeToC",method:"POST",params:{mobile:s}}),e=s=>a({url:"/api/mall/app/v2/login/toCSendSmsCode",method:"POST",params:{mobile:s}}),o=(s,e)=>a({url:"/api/app/insure/insurance/checkSmsCode",method:"POST",params:{mobile:s,smsCode:e}});export{e as a,o as c,s};
