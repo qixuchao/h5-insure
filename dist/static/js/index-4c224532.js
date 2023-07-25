0d930372df36c0f4de7a5c6dfcf38d63000e81f7
wuchao <wuchao@zhongan.io>
2023-07-25 14:49:30 +0800

    feat: 编译

---
+++ dist/static/js/index-4c224532.js
@@ -0,0 +1,1 @@
+import{d as a,b as s,c,i as d,$ as t}from"./index-2093395b.js";const e={class:"product-desc-wrap"},o={class:"product-name"},r={class:"product-desc"},p=a({name:"productDesc"}),u=a({...p,props:{dataSource:{default:()=>({})}},setup:a=>(a,p)=>(s(),c("div",e,[d("div",o,t(a.dataSource.title),1),d("p",r,t(a.dataSource.productDesc),1)]))});export{u as default};
