4da8c9928b8c2683db453f8185ea9f9e1ab9cef9
za-qixuchao <qixuchao@zhongan.com>
2023-10-16 22:31:48 +0800

    feat: npm run build

---
+++ dist/static/js/index-1f8ec0fb.js
@@ -0,0 +1,1 @@
+import{d as a,o as s,c as e,a as c,e as d}from"./index-bee60323.js";const t={class:"product-desc-wrap"},o={class:"product-name"},r={class:"product-desc"},p=a({name:"productDesc"}),u=a({...p,props:{dataSource:{default:()=>({})}},setup:a=>(a,p)=>(s(),e("div",t,[c("div",o,d(a.dataSource.title),1),c("p",r,d(a.dataSource.productDesc),1)]))});export{u as default};
---
+++ dist/static/js/isNaN-8652db43.js
@@ -0,0 +1,1 @@
+import{a_ as e,ce as r}from"./index-bee60323.js";function n(n){return function(n){return"number"==typeof n||e(n)&&"[object Number]"==r(n)}(n)&&n!=+n}export{n as i};
