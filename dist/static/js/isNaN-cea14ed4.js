4f9a4150af6d4ee12e846d3cd56c549bdf201844
za-qixuchao <qixuchao@zhongan.com>
2023-10-23 19:20:37 +0800

    feat: npm run build

--- dist/static/js/isNaN-cea14ed4.js
+++
@@ -1,1 +0,0 @@
-import{aZ as r,ck as e}from"./index-f3f205b3.js";function n(n){return function(n){return"number"==typeof n||r(n)&&"[object Number]"==e(n)}(n)&&n!=+n}export{n as i};
---
+++ dist/static/js/third-aa85af74.js
@@ -0,0 +1,1 @@
+import{a2 as a}from"./index-60c1582d.js";const s=s=>a({url:"/api/app/insure/insurance/customerList",method:"POST",data:s}),e=s=>a({url:"/api/app/insure/insurance/customerDetail",method:"POST",data:s});export{e as a,s as g};
