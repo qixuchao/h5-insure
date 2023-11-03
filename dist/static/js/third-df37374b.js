c21099c4af6d0f96df5cfdf03ba115a681544269
“liangxiaojun” <liangxiaojun@zhongan.io>
2023-11-03 16:36:03 +0800

    build: 构建发布

---
+++ dist/static/js/third-df37374b.js
@@ -0,0 +1,1 @@
+import{a2 as a}from"./index-e55a5aab.js";const e=e=>a({url:"/api/app/insure/insurance/customerList",method:"POST",data:e}),s=e=>a({url:"/api/app/insure/insurance/customerDetail",method:"POST",data:e});export{s as a,e as g};
