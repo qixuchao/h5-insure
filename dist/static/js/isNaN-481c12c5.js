ffb6cc53dbcbef200162334e0981d5124853a6d5
za-qixuchao <qixuchao@zhongan.com>
2023-11-06 15:55:22 +0800

    feat: npm run build

--- dist/static/js/isNaN-481c12c5.js
+++
@@ -1,1 +0,0 @@
-import{b0 as r,cl as e}from"./index-8fb28a8d.js";function n(n){return function(n){return"number"==typeof n||r(n)&&"[object Number]"==e(n)}(n)&&n!=+n}export{n as i};
--- dist/static/js/third-8f99712a.js
+++
@@ -1,1 +0,0 @@
-import{a2 as a}from"./index-8fb28a8d.js";const t=t=>a({url:"/api/app/insure/customer/customerList",method:"POST",data:t}),s=t=>a({url:"/api/app/insure/customer/customerDetail",method:"POST",data:t});export{s as a,t as g};
