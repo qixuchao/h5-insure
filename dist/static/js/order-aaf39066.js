e6ce1f684808092316321f803ec0f578fbd5b1fc
jjmwinner <jjmwinner@163.com>
2023-11-14 20:52:01 +0800

    build: deploy

---
+++ dist/static/js/order-aaf39066.js
@@ -0,0 +1,1 @@
+import{a2 as a}from"./index-29312576.js";const r=(r={})=>a({url:"/api/app/insure/customer/pageOrder",method:"POST",data:r}),e=(r={})=>a({url:"/api/app/insure/insurance/cancelOrder",method:"POST",data:r}),s=(r={})=>a({url:"/api/app/insure/insurance/orderInsureRecord",method:"POST",data:r});export{e as c,s as o,r as q};
