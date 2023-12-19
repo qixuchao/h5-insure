d30931fecf4d7f7aaeebc0538b4c9af3a47daf43
za-qixuchao <qixuchao@zhongan.com>
2023-12-19 14:11:40 +0800

    feat: npm run build

---
+++ dist/static/js/constants-b39aa53b.js
@@ -0,0 +1,1 @@
+const e={infoPreview:"/baseInsurance/free/infoPreview",faceVerify:"/baseInsurance/free/faceVerify",paymentResult:"/baseInsurance/orderDetail"};export{e as P};
---
+++ dist/static/js/face_img-b678980a.js
@@ -0,0 +1,1 @@
+var a="/static/png/face_img-4ac5b4e1.png";export{a as f};
