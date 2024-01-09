bcd952e62be9bb85c554650e84b28373871309c2
za-qixuchao <qixuchao@zhongan.com>
2024-01-09 15:35:16 +0800

    feat: npm run build

--- dist/static/js/notice-6a2a0f51.js
+++
@@ -1,1 +0,0 @@
-import{ae as a}from"./index-343d81ae.js";const l={HOlDER:1,INSURED:2,AGENT:3},e=a([{label:"投保人",value:l.HOlDER},{label:"被保人",value:l.INSURED},{label:"代理人",vale:l.AGENT}]),N={NEW_CONTRACT:1,VISIT:2,RISK_NOTIFICATION:3};export{l as N,N as Q,e as a};
---
+++ dist/static/js/third-318d3eb8.js
@@ -0,0 +1,1 @@
+import{ab as a}from"./index-4a902c97.js";const t=t=>a({url:"/api/app/insure/customer/customerList",method:"POST",data:t}),s=t=>a({url:"/api/app/insure/customer/customerDetail",method:"POST",data:t});export{t as a,s as g};
