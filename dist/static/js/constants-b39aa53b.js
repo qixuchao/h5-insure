fafefa4524654b7f41ad09803c7f897e5514dc5c
za-qixuchao <qixuchao@zhongan.com>
2024-02-05 10:08:49 +0800

    feat: resolve conflict

--- dist/static/js/constants-b39aa53b.js
+++
@@ -1,1 +0,0 @@
-const e={infoPreview:"/baseInsurance/free/infoPreview",faceVerify:"/baseInsurance/free/faceVerify",paymentResult:"/baseInsurance/orderDetail"};export{e as P};
--- dist/static/js/propasal-d633d7a0.js
+++
@@ -1,1 +0,0 @@
-const o={WORD:1,IMAGE:2,VIDEO:3};export{o as C};
