f4a4544ab7954e48f279d74879aadc7ad8dd0e4f
za-qixuchao <qixuchao@zhongan.com>
2024-02-23 15:50:54 +0800

    feat: npm run build

---
+++ dist/static/js/index-864c3338.js
@@ -0,0 +1,1 @@
+import{_ as e,d as s,o as a,c as t,a as d,e as i,g as o}from"./index-f0eeebf3.js";const l={class:"com-custom-card"},c={class:"title"};var r=e(s({__name:"index",props:{title:{type:String,required:!0}},setup:e=>(s,r)=>(a(),t("div",l,[d("div",c,i(e.title),1),d("div",null,[o(s.$slots,"default",{},void 0,!0)])]))}),[["__scopeId","data-v-059c21ee"]]);export{r as default};
--- dist/static/js/third-b16ee4be.js
+++
@@ -1,1 +0,0 @@
-import{ab as a}from"./index-121bf526.js";const t=t=>a({url:"/api/app/insure/customer/customerList",method:"POST",data:t}),s=t=>a({url:"/api/app/insure/customer/customerDetail",method:"POST",data:t});export{t as a,s as g};
