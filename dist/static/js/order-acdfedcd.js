a8c58277b49dc4fa23508798501d27efe8523cc9
za-qixuchao <qixuchao@zhongan.com>
2024-03-07 15:47:33 +0800

    feat: npm run build

---
+++ dist/static/js/order-acdfedcd.js
@@ -0,0 +1,1 @@
+import{ab as a}from"./index-8e0def42.js";const e=(e={})=>a({url:"/api/app/insure/customer/pageOrder",method:"POST",data:e}),r=(e={})=>a({url:"/api/app/insure/insurance/cancelOrder",method:"POST",data:e}),s=(e={})=>a({url:"/api/app/insure/insurance/orderInsureRecord",method:"POST",data:e});export{r as c,s as o,e as q};
