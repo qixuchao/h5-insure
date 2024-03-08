008d58bdf516a30d1ae8fcdf42cf8df7a64b2db6
za-qixuchao <qixuchao@zhongan.com>
2024-03-08 22:15:16 +0800

    feat: npm run build

---
+++ dist/static/js/scribing-90355cf7.js
@@ -0,0 +1,1 @@
+import{ab as a}from"./index-169b3a9c.js";const i=(i={})=>a({url:"/api/app/insure/insurance/confirmRiskTranscription",method:"POST",data:i}),s=(i={})=>a({url:"/api/app/insure/insurance/queryRiskTranscriptionResult",method:"POST",data:i}),r=(i={})=>a({url:"/api/app/insure/insurance/saveRiskTranscription",method:"POST",data:i});export{i as c,s as q,r as s};
