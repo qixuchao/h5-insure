e0ffadafe770a583c5c6dc29de6324ed76be7e39
za-qixuchao <qixuchao@zhongan.com>
2024-03-11 18:16:12 +0800

    feat: npm run build

---
+++ dist/static/js/third-361721c4.js
@@ -0,0 +1,1 @@
+import{ab as a}from"./index-fbb8a705.js";const t=t=>a({url:"/api/app/insure/customer/customerList",method:"POST",data:t}),s=t=>a({url:"/api/app/insure/customer/customerDetail",method:"POST",data:t});export{t as a,s as g};
